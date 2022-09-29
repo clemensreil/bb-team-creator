07196b423085aa00ac9ecb8afc548719c146ad19
Clemens Reil <cdiezinger@yahoo.de>
2022-09-29 22:11:41 +0200

    basic form inputs for config section finished

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
     <Config>
       <TreasuryInput />
       <RaceDropdown />
+      <TeamNameInput />
+      <CoachNameInput />
+      <NafNumberInput />{" "}
     </Config>
   );
 }
@@ -16,7 +19,7 @@ type ConfigProps = {
 };
 
 function Config({ children }: ConfigProps) {
-  return <div className="flex gap-2">{children}</div>;
+  return <div className="flex gap-2 flex-wrap">{children}</div>;
 }
 
 function TreasuryInput() {
@@ -60,7 +63,71 @@ function RaceDropdown() {
         <option value="black-orc">Black Orc</option>
         <option value="chaos-chosen">Chaos Chosen</option>
         <option value="chaos-dwarf">Chaos Dwarf</option>
+        <option value="Necromantic Horror">Necromantic Horror</option>
       </select>
+    </div>
+  );
+}
+
+function TeamNameInput() {
+  const [teamName, setTeamName] = useState("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTeamName(event.target.value);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <label htmlFor="coach-name">Set Team Name</label>
+      <input
+        type="text"
+        id="coach-name"
+        className="shadow appearance-none border rounded py-2 px-3"
+        value={teamName}
+        onChange={handleChange}
+      ></input>
+    </div>
+  );
+}
+
+function CoachNameInput() {
+  const [coachName, setCoachName] = useState("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCoachName(event.target.value);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <label htmlFor="coach-name">Set Coach Name</label>
+      <input
+        type="text"
+        id="coach-name"
+        className="shadow appearance-none border rounded py-2 px-3"
+        value={coachName}
+        onChange={handleChange}
+      ></input>
+    </div>
+  );
+}
+
+function NafNumberInput() {
+  const [NafNumber, setNafNumber] = useState("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNafNumber(event.target.value);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <label htmlFor="coach-name">Set Naf Number</label>
+      <input
+        type="text"
+        id="coach-name"
+        className="shadow appearance-none border rounded py-2 px-3"
+        value={NafNumber}
+        onChange={handleChange}
+      ></input>
     </div>
   );
 }
